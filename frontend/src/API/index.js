ef5786b62003a3d6187c41b0fd6e895ecc512973
John Michael Julve <julvej0@gmail.com>
2024-02-17 11:24:42 +0800

    change api link

--- frontend/src/API/index.js
+++ frontend/src/API/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url = "http://localhost:5001/api/"
+// const url = "http://localhost:5001/api/"
+const url = "https://auxilium.onrender.com/api/"
 
 export const client = axios.create({
     baseURL: url,
--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
-import { isRouteErrorResponse } from "react-router-dom";
 import { client } from "../API";
 import { useUserContext } from "./userUserContext";
 
 export const useSignup = () => {
 	const [error, setError] = useState(null);
 	const [isLoading, setIsLoading] = useState(null);
-	const { dispatch } = useUserContext();
 
 	const signup = async (name, email, password) => {
 		setIsLoading(true);
 		setError(null);
 
-		const response = await client
+		await client
 			.post("/user/signup", { name, email, password })
 			.catch((err) => {
                 setIsLoading(false)
