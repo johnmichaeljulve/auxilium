9a9d9fead2b3b3b102a5e2ac746f7fcdf95a4341
John Michael Julve <julvej0@gmail.com>
2024-02-24 13:38:51 +0800

    api

--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -4,3 +4,4 @@ export const PORT = process.env.PORT || 5000
 export const NODE_ENV = process.env.NODE_ENV || 'development'
 export const MONGO_URI = process.env.MONGO_URI || null
 export const SECRET = process.env.SECRET || null
+export const API = process.env.API || null
--- frontend/src/API/index.js
+++ frontend/src/API/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import {API} from "../../../backend/utils/config"
 
-const url = "http://localhost:5001/api/"
-// const url = "https://auxilium.onrender.com/api"
+// const url = "http://localhost:5001/api/"
+const url = API
 
 export const client = axios.create({
     baseURL: url,
